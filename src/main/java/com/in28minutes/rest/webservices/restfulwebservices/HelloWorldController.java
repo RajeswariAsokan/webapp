f4219dd74e7dd4d23117e5bcbdeb24fb8e6d71ec
RajeswariAsokan <47206071+RajeswariAsokan@users.noreply.github.com>
2023-04-14 01:05:51 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/")
 	public String helloWorld() {
-		return "{\"message\":\"Hello World Java version 4.10 \"}";
+		return "{\"message\":\"Hello World Java version 4.20 \"}";
 	}
 }
